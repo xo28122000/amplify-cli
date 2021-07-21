9db19e0e21a46c343caf9a8649cce5cea5f7aa8c
Jainam Shah <jainam.2000@yahoo.com>
2021-07-21 15:17:12 -0700

    feat: hooks execution

--- packages/amplify-cli-core/src/index.ts
+++ packages/amplify-cli-core/src/index.ts
@@ -22,7 +22,7 @@ export * from './utils';
 export * from './banner-message';
 export * from './cliGetCategories';
 export * from './cliRemoveResourcePrompt';
-export * from "./cliViewAPI";
+export * from './hooksExecutioner';
 
 // Temporary types until we can finish full type definition across the whole CLI
 
